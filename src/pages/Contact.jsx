3fa96df661edb59da28cefec3d62435028dd98fc
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-05-10 02:12:29 -0700

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Product = () => {
+const Contact = () => {
     return (
         <div>
             <h1>Contact page</h1>
@@ -8,4 +8,4 @@ const Product = () => {
     );
 };
 
-export default Product;
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-const Dashboard = () => {
+const Home = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <Row className="piclocation">
+                <Col lg={6}>
+                    <img className='picborderradius img-fluid picsize' src={require('../Assets/pictest.png')} alt="Logo" width={450} />
+                </Col>
+
+                <Col lg={6} className="d-flex align-items-center">
+                    <p className='maintextloc'>HI THERE I'M</p>
+                    <p className='titleloc'><span className='blue'>Ulises</span> Ortega</p>
+                </Col>
+
+                <Col>
+                    <p className='summary'>Jr. Web Developer </p>
+                </Col>
+            </Row>
         </div>
     );
 };
 
-export default Dashboard;
+export default Home;
